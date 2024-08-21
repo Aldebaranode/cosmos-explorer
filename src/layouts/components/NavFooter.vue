8d1e70753872fb11d1c7c0d90f0e8da244485ff2
syahidnurrohim <syahidnurrohim@gmail.com>
2024-08-21 21:09:44 +0700

    update footer

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -2,15 +2,15 @@
   <!-- footer -->
   <div class="h-[1px] w-full bg-gray-100 mt-6 dark:bg-[#384059]"></div>
   <footer
-    class="flex items-center flex-col gap-2 md:flex-row text-sm light:bg-gray-100 dark:bg-dark py-6 z-10 px-8 w-full rounded-lg">
+    class="flex items-center flex-col md:!flex-row gap-2 text-sm light:bg-gray-100 dark:bg-dark py-6 z-10 px-8 w-full rounded-lg">
     <div class="block md:!hidden flex flex-1">
       &copy;
       {{ new Date().getFullYear() }}&nbsp;
     </div>
     <div class="flex flex-1 gap-1">
+      License &copy;
       <a class="link link-primary no-underline" href="https://github.com/ping-pub/explorer/blob/master/LICENSE"
-        target="_blank" rel="noopener noreferrer">License</a> &copy;
-      Ping.pub {{ new Date().getFullYear() }}&nbsp;
+        target="_blank" rel="noopener noreferrer">Ping.pub</a> {{ new Date().getFullYear() }}&nbsp;
     </div>
     <div class="flex flex-1">
       Maintained By&nbsp;
